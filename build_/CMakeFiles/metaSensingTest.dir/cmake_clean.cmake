0ecf3b55de3a628e31ed0dc8c2883e1644eaa7d5
Astroteo <matteob.86@gmail.com>
2020-10-09 17:02:41 +0200

    finalized => error corrected

---
+++ build_/CMakeFiles/metaSensingTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/metaSensingTest.dir/main.cpp.o"
+  "metaSensingTest"
+  "metaSensingTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/metaSensingTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
